c3decbd67783b7250d8f229afe173ee5c3333e3d
simran <simran@gmail.com>
2025-08-05 11:51:48 +0530

    refactor: update MongoDB URI in .env and clean up index.js

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const express = require('express');
 const connectDB = require('./models/connection');
 const app = express();
 
-// Git configuration
-const { exec } = require('child_process');
-exec('git config --global user.name "simran"');
-exec('git config --global user.email "simran@gmail.com"');
-
-// Connect to DB, then start server
-connectDB()
-
+// Connect to DB, then start 
+// server
 const PORT = process.env.PORT || 4000;
 
+connectDB()
+  .then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on port http://localhost:${PORT}`);
     });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1);
+  });
  
 
